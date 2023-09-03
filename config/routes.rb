7b9280464deb40b2973ce7da879f5c54d318cecd
Raymond Lin <raymond.lin.7690@gmail.com>
2023-09-02 23:21:20 -0700

    Adding Admin Categories"

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   get '/about', to: "about#index"
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -16,6 +17,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
