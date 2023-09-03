567025c140fa09f340bea108172ec59c27629bff
Raymond Lin <raymond.lin.7690@gmail.com>
2023-09-03 00:20:18 -0700

    Adding user-auth

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,13 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
+  get '/signup' => 'user#new'
+  post '/users' => 'user#create' 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
